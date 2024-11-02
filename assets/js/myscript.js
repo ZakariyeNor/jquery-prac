2b9f05b04d5c5e5e21ad0a8a1deee5a1ea4c64ca
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-11-02 09:22:06 +0000

    add myscript style

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
-    
+    $("h2").css("text-decoration", "underline"); // Underline all <h2> elements.
+    $("ul").css("border", "solid 1px #ccc"); // Add border to all list items.
+
 });
